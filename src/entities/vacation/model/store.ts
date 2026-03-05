3284c8b870125f4884bcfc36c78eab6e4d4eecc1
sobolevaea <liziaka@mail.ru>
2026-03-05 09:30:50 +0300

    feat: add store

---
+++ src/entities/vacation/model/store.ts
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+
+interface VacationState {
+  name: string;
+  country: string;
+  resort: string;
+  program: string;
+
+  setName: (name: string) => void;
+  setCountry: (country: string) => void;
+  setResort: (resort: string) => void;
+  setProgram: (program: string) => void;
+}
+
+export const useVacationStore = create<VacationState>((set) => ({
+  name: "",
+  country: "",
+  resort: "",
+  program: "",
+
+  setName: (name) => set({ name }),
+
+  setCountry: (country) =>
+    set({
+      country,
+      resort: "",
+      program: "",
+    }),
+
+  setResort: (resort) =>
+    set({
+      resort,
+      program: "",
+    }),
+
+  setProgram: (program) => set({ program }),
+}));
